9af6ed81a50335af12034c3809575222747c8d97
Emi <graficoemiliano@gmail.com>
2021-08-04 19:06:04 -0400

    Nuevo despliegue

---
+++ precache-manifest.7dd8c8567c40734e10716dfc9c078c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee24fdf1b9b761310dc9",
+    "url": "/calculator-app/css/app.836b2495.css"
+  },
+  {
+    "revision": "17d8abb6b8483a705be1323a165c0c77",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "ee24fdf1b9b761310dc9",
+    "url": "/calculator-app/js/app.bdf93c59.js"
+  },
+  {
+    "revision": "e40d4ea1a044228334d5",
+    "url": "/calculator-app/js/chunk-vendors.0b1ddb48.js"
+  },
+  {
+    "revision": "e0081555e6e0492a037499d69a0f7c95",
+    "url": "/calculator-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/calculator-app/robots.txt"
+  }
+]);
